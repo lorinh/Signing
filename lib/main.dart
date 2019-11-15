5fd365ce8004a486968ad8de13d902009327ed41
Lorin Hershberger <lorinhersh@gmail.com>
2019-11-14 23:32:18 -0500

    Changed from pdf to png

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,5 @@
-import 'dart:async';
-import 'dart:io';
-
-import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_full_pdf_viewer/full_pdf_viewer_scaffold.dart';
-import 'package:path_provider/path_provider.dart';
-
-import 'package:flutter/services.dart' show rootBundle;
-import 'dart:typed_data';
+import 'package:photo_view/photo_view.dart';
 
 void main() {
   runApp(MaterialApp(
@@ -22,72 +14,76 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
-  String pathPDF = "";
-
-  @override
-  void initState() {
-    super.initState();
-    createFileOfPdfUrl().then((f) {
-      setState(() {
-        pathPDF = f.path;
-        print(pathPDF);
-      });
-    });
-  }
-
-  Future<File> createFileOfPdfUrl() async {
-    Directory tempDir = await getTemporaryDirectory();
-    String tempPath = tempDir.path;
-    File tempFile = File('$tempPath/copy.pdf');
-    ByteData bd = await rootBundle.load('assets/Waiver.pdf');
-    await tempFile.writeAsBytes(bd.buffer.asUint8List(), flush: true);
-    return tempFile;
-    // final url = "http://africau.edu/images/default/sample.pdf";
-    // final filename = url.substring(url.lastIndexOf("/") + 1);
-    // var request = await HttpClient().getUrl(Uri.parse(url));
-    // var response = await request.close();
-    // var bytes = await consolidateHttpClientResponseBytes(response);
-    // String dir = (await getApplicationDocumentsDirectory()).path;
-    // File file = new File('$dir/$filename');
-
-    //await file.writeAsBytes(bytes);
-    File file = new File("/assets/Waiver.pdf");
-    return file;
-  }
-
   @override
   Widget build(BuildContext context) {
-    print(pathPDF);
-    return pathPDF != "" ? PDFViewerScaffold(
-        appBar: AppBar(
-          title: Text("Document"),
-          actions: <Widget>[
-            IconButton(
-              icon: Icon(Icons.share),
-              onPressed: () {},
-            ),
-          ],
-        ),
-        path: pathPDF) : Text("Loading...");
+    return Container(
+      child: PhotoView(
+        imageProvider: AssetImage("assets/Waiver.png"),
+      ),
+    );
   }
-}
 
-class PDFScreen extends StatelessWidget {
-  String pathPDF = "";
-  PDFScreen(this.pathPDF);
-
-  @override
-  Widget build(BuildContext context) {
-    return PDFViewerScaffold(
-        appBar: AppBar(
-          title: Text("Document"),
-          actions: <Widget>[
-            IconButton(
-              icon: Icon(Icons.share),
-              onPressed: () {},
-            ),
-          ],
-        ),
-        path: pathPDF);
-  }
+  // @override
+  // Widget build(BuildContext context) {
+  //   print(pathPDF);
+  //   return pathPDF != ""
+  //       ? Stack(
+  //           children: [
+  //             // Column(
+  //             //   mainAxisSize: MainAxisSize.max,
+  //             //   mainAxisAlignment: MainAxisAlignment.end,
+  //             //   children: [
+  //             //     Container(
+  //             //       color: Colors.red,
+  //             //       height: 50.0,
+  //             //       width: 50,
+  //             //       //constraints: BoxConstraints.expand(),
+  //             //     ),
+  //             //   ],
+  //             // ),
+  //             PDFViewerScaffold(
+  //               appBar: AppBar(
+  //                 title: Text("Document"),
+  //                 actions: <Widget>[
+  //                   IconButton(
+  //                     icon: Icon(Icons.share),
+  //                     onPressed: () {},
+  //                   ),
+  //                 ],
+  //               ),
+  //               path: pathPDF,
+  //             ),
+  //             Container(
+  //               color: Colors.white,
+  //             ),
+  //             // Container(
+  //             // color: Colors.red,
+  //             //child: Text("This is normally a name"),
+  //             // height: 100,
+  //             //padding: EdgeInsets.all(20),
+  //             //height: 50.0,
+  //             //width: 50,
+  //             //alignment: Alignment(100, -5000000),
+  //             //constraints: BoxConstraints.expand(),
+  //             // ),
+  //             Column(
+  //               mainAxisSize: MainAxisSize.max,
+  //               mainAxisAlignment: MainAxisAlignment.end,
+  //               children: [
+  //                 Container(
+  //                   color: Colors.red,
+  //                   height: 50.0,
+  //                   //constraints: BoxConstraints.expand(),
+  //                 ),
+  //               ],
+  //             ),
+  //           ],
+  //         )
+  //       : Scaffold(
+  //           appBar: AppBar(
+  //             title: Text("Document"),
+  //           ),
+  //           body: Center(child: CircularProgressIndicator()),
+  //         );
+  // }
 }
